7231bc0018b0ad3a709b537954673baff5e6022b
Jerred Shepherd <ShepherdJerred@gmail.com>
2021-01-02 13:25:13 -0800

    Impl

--- ShepherdJerred/Plugin.cs
+++ ShepherdJerred/Plugin.cs
@@ -1,33 +1,19 @@
 using System.Threading.Tasks;
 using Impostor.Api.Plugins;
-using Microsoft.Extensions.Logging;
 
-namespace Commands.ShepherdJerred
-{
+namespace Commands.ShepherdJerred {
     [ImpostorPlugin(
         "com.shepherdjerred",
         "Commands",
         "Jerred Shepherd",
         "1.0.0")]
     // ReSharper disable once UnusedType.Global
-    public class Main : PluginBase
-    {
-        private readonly ILogger<Main> _logger;
-
-        public Main(ILogger<Main> logger)
-        {
-            _logger = logger;
-        }
-
-        public override ValueTask EnableAsync()
-        {
-            _logger.LogInformation("Example is being enabled.");
+    public class Main : PluginBase {
+        public override ValueTask EnableAsync() {
             return default;
         }
 
-        public override ValueTask DisableAsync()
-        {
-            _logger.LogInformation("Example is being disabled.");
+        public override ValueTask DisableAsync() {
             return default;
         }
     }
--- ShepherdJerred/Startup.cs
+++ ShepherdJerred/Startup.cs
@@ -3,16 +3,13 @@ using Impostor.Api.Plugins;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Commands.ShepherdJerred
-{
-    public class Startup : IPluginStartup
-    {
-        public void ConfigureHost(IHostBuilder host)
-        {
+namespace Commands.ShepherdJerred {
+    public class Startup : IPluginStartup {
+        public void ConfigureHost(IHostBuilder host) {
         }
 
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddSingleton<IEventListener, CommandListener>();
         }
     }
 }
